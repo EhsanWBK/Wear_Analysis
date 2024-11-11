e519080efd47a49a5f9e50e799d93b3068574c8e
Florian Schindler <uijeb@student.kit.edu>
2024-11-11 23:56:46 +0100

    Small Final Changes

--- opcServer.py
+++ opcServer.py
@@ -1,30 +1,11 @@
-''' '''
-
-from time import sleep
 from opcua.server.server import Server 
-from opcua.ua import ObjectIds, DataValue, Variant, VariantType
-import cv2
-import base64
-from sys import exit
-import threading as th
-try: 
-    import neoapi  
-    import vax_io   
-except:
-    print('Cannot import API.')
-
+from opcua.ua import DataValue, Variant, VariantType
+import cv2; import base64
+import neoapi; import vax_io   
 
 # Server setup
-ENDPOINT = "opc.tcp://141.3.142.81:12345" # CHANGE???
-# ENDPOINT = 'opc.tcp://127.0.0.1:12345'
+ENDPOINT = "opc.tcp://141.3.142.81:12345" 
 NAMESPACE = 'CameraSpace'
-
-# Camera setup
-CAM_PIXEL_FORMAT = 'Mono8'
-IS_COLOR = True
-
-stopEvent = th.Event()
-triggerSet = th.Event()
 
 class CamServer:
     def __init__(self) -> None:
@@ -74,7 +55,6 @@ class Baumer():
         elif self.camera.f.PixelFormat.GetEnumValueList().IsReadable('Mono8'):
             self.camera.f.PixelFormat.SetString('Mono8'); print('Mono8')
         else: print('No supported pixel format')
-        self.camera.f.PixelFormat.SetString(CAM_PIXEL_FORMAT)
         self.camera.f.ExposureTime.Set(10000)
         self.camera.f.AcquisitionFrameRateEnable.value = True
         self.camera.f.AcquisitionFrameRate.value = 10
@@ -84,9 +64,11 @@ class Baumer():
         
     def startCam(self):
         print('Starting Stream')
-        for cnt in range(0,200):
-            self.img = self.camera.GetImage().GetNPArray()
-            
+        for cnt in range(0,200): self.img = self.camera.GetImage().GetNPArray()
+
+    def stopCam(self):
+        print('Disconnecting Camera.'); self.camera.Disconnect()
+        
     def getFrame(self) -> bytes:
         self.img = self.camera.GetImage().GetNPArray()
         ret, jpeg = cv2.imencode('.jpg', self.img)
@@ -108,11 +90,11 @@ class Baumer():
         if triggerImg.shape == (0,0,1): return False, None
         elif triggerImg.shape == (2048, 2448, 1): 
             print('Trigger received.')
-            ret, jpeg = cv2.imencode('.jpg', triggerImg)
+            _, jpeg = cv2.imencode('.jpg', triggerImg)
             jpegString = base64.b64encode(jpeg).decode('utf-8')
             return True, jpegString
         
-def castImage():
+def run():
     cam = Baumer(); camServer = CamServer()
     serverOnline = camServer.startServer()
     triggerSignal = camServer.checkTrigger()
@@ -132,7 +114,8 @@ def castImage():
                 cam.triggerModeOff(); print('Trigger Signal: ', triggerSignal)
     finally:
         triggerMode = cam.triggerModeOff()
+        cameraOnline = cam.stopCam()
         serverOnline = camServer.stopServer()
 
-if __name__ == '__main__': castImage()
+if __name__ == '__main__': run()
     
