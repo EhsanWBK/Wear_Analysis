e8cdfae0a2b0ec942ac9bc8d0d85572bcededb39
Florian Schindler <uijeb@student.kit.edu>
2024-11-11 22:29:40 +0100

    Final Version: Folder Structure Check

--- code/test.py
+++
@@ -1,8 +0,0 @@
-# from postProcessing import testDBSAN
-
-# testDBSAN()
-
-from modelTraining import evalModelTraining
-from os.path import join
-from os import getcwd
-evalModelTraining(savePath=join(getcwd(),'models','milling','milling_2','With_TP'))
