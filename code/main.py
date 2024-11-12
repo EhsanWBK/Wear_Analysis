ca30e49befc6d030dee13657fb76497f1a396250
Florian Schindler <uijeb@student.kit.edu>
2024-11-12 18:10:44 +0100

    Hotfix

--- code/main.py
+++ code/main.py
@@ -85,9 +85,11 @@ def observeTrigger(event, stopEvent):
         if event.is_set() and not stopEvent.is_set(): videoCam.triggerModeOn()
         while event.is_set() and not stopEvent.is_set():
             # if videoCam.triggerStatus(): 
-            frame = videoCam.getFrame(); triggerTemp.append(deepcopy(frame))
-            blob = reformatFrame(frame=frame)
-            eel.updateCanvas2(blob)()
+            try:
+                frame = videoCam.getFrame(); triggerTemp.append(deepcopy(frame))
+                blob = reformatFrame(frame=frame)
+                eel.updateCanvas2(blob)()
+            except:pass
             # if not videoCam.triggerStatus() and triggerTemp != []:
             #     saveFolder = saveTrigger(triggerTemp); triggerTemp = []
             #     try: segmentDataStack(dataPath=saveFolder, model=currentModel, savePath=saveFolder)
